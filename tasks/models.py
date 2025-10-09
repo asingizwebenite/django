e5b350954f53edfd5b9466c912b853fce3f58b5b
asingizwebenite <asingizwebenite2008@gmail.com>
2025-10-09 21:40:37 +0200

    created the task model

---
+++ tasks/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description=models.TextField(blank=true)
+    created_at = models.DateTimeField(auto_now_add=true)
+    completed = models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return sel.title
