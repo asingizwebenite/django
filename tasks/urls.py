567aae320b7f4162513f8e1e26ee324c6ade3b0b
asingizwebenite <asingizwebenite2008@gmail.com>
2025-10-10 13:19:27 +0200

    implemented some views

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns=[
     path('', views.dashboard, name='home'),
-    path('login/', views.user_login, name='login')
+    path('login/', views.user_login, name='login'),
+    path('signup/', views.user_signup, name='signup'),
+    path('dashboard/',views.dashboard, name='dashboard')
 ]
