2418223a21b7e9bec9611f6cdc9d776af7716cdd
asingizwebenite <asingizwebenite2008@gmail.com>
2025-10-10 13:15:03 +0200

    implemented some views

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Task
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def landing(request):
+    return render(request, tasks)
+
+@login_required
+def dashboard(request):
+    tasks = Task.objects.all()
+    return render(request, 'tasks/dashboard.html',{'tasks':tasks})
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request,username=username, password=password )
+
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')
+        
+       
+    return render(request, 'tasks/login.html')
+   
+
+
+def user_signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(dashboard)
+    else:
+        form = UserCreationForm()
+    return redirect(request, 'login.html', {'form':form})
+
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('/')
+
+
+
+
+
+
+
